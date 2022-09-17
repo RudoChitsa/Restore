790497a74cb87df065364cb82b6388eb1f4c104a
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-09-17 08:02:16 +0200

    Parallax

--- js/parallax.js-1.3.1/parallax.js
+++ js/parallax.js-1.3.1/parallax.js
@@ -1,13 +1,7 @@
-/*!
- * parallax.js v1.3.1 (http://pixelcog.github.io/parallax.js/)
- * @copyright 2015 PixelCog, Inc.
- * @license MIT (https://github.com/pixelcog/parallax.js/blob/master/LICENSE)
- */
 
 ;(function ( $, window, document, undefined ) {
 
-  // Polyfill for requestAnimationFrame
-  // via: https://gist.github.com/paulirish/1579671
+ 
 
   (function() {
     var lastTime = 0;
